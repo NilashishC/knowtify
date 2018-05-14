c60917f453b6740403f75ebeff08cb7397d5be84
NilashishC <nilashishchakraborty8@gmail.com>
2018-05-14 19:30:28 +0530

    Added more logging

--- knowtify.py
+++ knowtify.py
@@ -17,9 +17,11 @@ import email
 class Knowtify:
 
     """
+
         This class implements Knowtify. For each (email, mobile number) pair
         in the input JSON file, main.py would create an object of this class
         and call notifier().
+
     """
 
     def __init__(self, host, email, mobile, passwd, mailbox):
@@ -37,11 +39,11 @@ class Knowtify:
                 self.__isValidInput = True
             else:
                 # Log invalid mobile
-                print('Invalid Mobile')
+                print('Invalid Mobile Format')
                 self.__isValidInput = False
         else:
             # Log invalid email
-            print('Invalid Email')
+            print('Invalid Email Format')
             self.__isValidInput = False
 
     def __sanitizeMobile(self, email):
@@ -62,8 +64,8 @@ class Knowtify:
 
         if self.__isValidInput:
 
-            # Log that validation was successful
-            print ('Email & Number Validation Successful...')
+            # Log that format validation was successful
+            print ('Email & Number Format Validation Successful...')
             rv, unread = 0, 0
 
             try:
@@ -73,29 +75,32 @@ class Knowtify:
                 rv, unread = mail.search(None, 'unSeen')
 
             except imaplib.IMAP4.error as e:
+                # Log the exception encountered
                 print("Exception Encountered : {0}".format(e))
 
             else:
                 # Log that 'Login was Successful'
-                print('Login Successful...')
-                print('---------------------------------------')
+                print('Login & Mail Box Selection Successful...')
+
                 if rv == 'OK':
+
+                    # Log RV for Mail Box search returned OK
+                    print("RV for mail box search returned OK")
+                    print('---------------------------------------')
+
                     for e_num in unread[0].split():
                         rv, data = mail.fetch(e_num, '(RFC822)')
-
-                        # raw_email = data[0][1]
-                        # print(raw_email)
                         email_decoded = data[0][1].decode("utf-8")
-                        # print(email_decoded)
                         email_msg = email.message_from_string(email_decoded)
-                        # print(email_message)
+
+                        # Send SMS to the corresponding mobile number
                         print('FROM : {0}'.format(email_msg['FROM']))
                         print('SUBJECT : {0}'.format(email_msg['SUBJECT']))
                         print('DATE : {0}'.format(email_msg['DATE']))
                         print('----------------------------------------------')
                 else:
                     # Log rv was not 'OK'
-                    print("RV was not OK")
+                    print("RV for mail box search did not return OK")
                     pass
 
         else:
@@ -103,3 +108,7 @@ class Knowtify:
             # no action was taken
             print("No further action was taken because email or Mobile \
                   validation failed")
+
+    def __sendSMS(self, from, subject, data):
+        # Send an SMS to the corresponding number
+        pass
